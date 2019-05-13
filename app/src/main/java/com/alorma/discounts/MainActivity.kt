093fc70cd23c560b5f5c8df663a93ff11b272c64
Bernat Borrás <bernatbor15@gmail.com>
2019-05-14 00:15:48 +0200

    Layout for discounts list

--- app/src/main/java/com/alorma/discounts/MainActivity.kt
+++ app/src/main/java/com/alorma/discounts/MainActivity.kt
@@ -2,18 +2,13 @@ package com.alorma.discounts
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.alorma.discounts.ui.camera.CameraFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, CameraFragment.newInstance())
-                .commitNow()
-        }
+
     }
 
 }
--- app/src/main/java/com/alorma/discounts/ui/camera/CameraViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.alorma.discounts.ui.camera
-
-import androidx.lifecycle.ViewModel
-
-class CameraViewModel : ViewModel() {
-
-}
