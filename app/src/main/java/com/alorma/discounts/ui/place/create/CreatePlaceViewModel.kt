e89cef9f2a0c40fb4f14d7ad946bfe63b737f9bb
Bernat Borrás <bernatbor15@gmail.com>
2019-06-02 19:09:08 +0200

    Add create place

---
+++ app/src/main/java/com/alorma/discounts/ui/place/create/CreatePlaceViewModel.kt
@@ -0,0 +1,9 @@
+package com.alorma.discounts.ui.place.create
+
+import com.alorma.discounts.ui.base.BaseViewModel
+
+class CreatePlaceViewModel : BaseViewModel<CreatePlaceViewModel.View>() {
+
+
+    interface View : BaseView
+}
--- app/src/main/java/com/alorma/discounts/ui/place/create/NewPlaceFragment.kt
+++ app/src/main/java/com/alorma/discounts/ui/place/create/NewPlaceFragment.kt
@@ -1,13 +1,35 @@
 package com.alorma.discounts.ui.place.create
 
 import android.os.Bundle
-import android.widget.Toast
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.alorma.discounts.databinding.CreatePlaceFragmentBinding
 import com.alorma.discounts.ui.base.BaseFragment
+import kotlinx.android.synthetic.main.create_place_fragment.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class NewPlaceFragment : BaseFragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-        Toast.makeText(requireContext(), "Create", Toast.LENGTH_SHORT).show()
+    private val createPlaceViewModel: CreatePlaceViewModel by viewModel()
+
+    private lateinit var binding: CreatePlaceFragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = CreatePlaceFragmentBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+        val appbarConfig = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appbarConfig)
+        toolbar.setOnMenuItemClickListener { it.onNavDestinationSelected(navController) }
     }
 }
