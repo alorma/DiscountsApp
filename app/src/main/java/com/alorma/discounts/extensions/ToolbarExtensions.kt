0e6c788cb1dfff9d39f830ab9b749e01d942bc31
Bernat Borrás <bernat.borras@schibsted.com>
2019-06-11 15:47:41 +0200

    Update nav,not working

--- app/src/main/java/com/alorma/discounts/extensions/ToolbarExtensions.kt
+++ app/src/main/java/com/alorma/discounts/extensions/ToolbarExtensions.kt
@@ -1,14 +1,25 @@
 package com.alorma.discounts.extensions
 
+import android.app.Activity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.findNavController
 import com.alorma.discounts.R
 
-fun Fragment.setupBack(toolbar: Toolbar) {
-    val navController = findNavController()
+fun Fragment.setupBack(toolbar: Toolbar, id: Int = R.id.nav_graph) {
     toolbar.setNavigationIcon(R.drawable.ic_arrow_back)
     toolbar.setNavigationOnClickListener {
-        navController.popBackStack()
+        requireActivity().navigateUp(id)
+    }
+}
+
+internal fun Fragment.navigateUp(id: Int = R.id.nav_graph) {
+    requireActivity().navigateUp()
+}
+
+internal fun Activity.navigateUp(id: Int = R.id.nav_graph) {
+    val successful = findNavController(id).popBackStack()
+    if (!successful) {
+        finish()
     }
 }
