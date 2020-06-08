dda794ae51d8f11ab05600642bea3f532c3c144e
Bernat Borrás <bernat.borras@adevinta.com>
2020-06-08 19:34:54 +0200

    Init

---
+++ android/app/src/main/kotlin/com/alorma/discounts/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alorma.discounts.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Discounts',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Discounts at your palm'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  void _scanBarcode() async {
+    var result = await BarcodeScanner.scan();
+
+    print(result.type);
+    print(result.rawContent);
+    print(result.format);
+    print(result.formatNote);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _scanBarcode,
+        tooltip: 'Scan',
+        child: Icon(Icons.scanner),
+      ),
+    );
+  }
+}
